0efc44afb476fb71cc4c9bdf7ce711cf2a18970d
Changweihua <Changweihua@outlook.com>
2012-11-28 16:51:04 +0800

    Usgae of RichEditor

---
+++ RichEditor/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RichEditor
+{
+    /*************************************************************************************
+     * CLR  版本:       4.0.30319.586
+     * 类 名 称:       Form1
+     * 机器名称:       HSERVER
+     * 命名空间:       RichEditor
+     * 文 件 名:       Form1
+     * 创建时间:       2012/11/28 16:06:46
+     * 作    者:       常伟华 Changweihua
+     * 签    名:       To be or not, it is not a problem !
+     * 网    站:       http://www.cmono.net
+     * 邮    箱:       changweihua@outlook.com
+     * 
+     * 修改时间:
+     * 修 改 人:
+     * 
+     ************************************************************************************/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
