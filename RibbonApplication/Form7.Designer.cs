14d59efe4e58631279ba4db248ad9200d9cc456f
Changweihua <Changweihua@outlook.com>
2012-11-28 11:00:52 +0800

    Usage of AlertControl

---
+++ RibbonApplication/Form7.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace RibbonApplication
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraBars.Alerter.AlertButton alertButton1 = new DevExpress.XtraBars.Alerter.AlertButton();
+            DevExpress.XtraBars.Alerter.AlertButton alertButton2 = new DevExpress.XtraBars.Alerter.AlertButton();
+            DevExpress.XtraBars.Alerter.AlertButton alertButton3 = new DevExpress.XtraBars.Alerter.AlertButton();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form7));
+            this.alertControl1 = new DevExpress.XtraBars.Alerter.AlertControl(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // alertControl1
+            // 
+            alertButton1.Hint = "Open";
+            alertButton1.ImageIndex = 0;
+            alertButton1.Name = "alertButton1";
+            alertButton2.Hint = "Save";
+            alertButton2.ImageIndex = 1;
+            alertButton2.Name = "alertButton2";
+            alertButton3.Hint = "Notes";
+            alertButton3.ImageIndex = 2;
+            alertButton3.Name = "alertButton3";
+            this.alertControl1.Buttons.Add(alertButton1);
+            this.alertControl1.Buttons.Add(alertButton2);
+            this.alertControl1.Buttons.Add(alertButton3);
+            this.alertControl1.Images = this.imageList1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "FileOpen.png");
+            this.imageList1.Images.SetKeyName(1, "FileSave.png");
+            this.imageList1.Images.SetKeyName(2, "GroupManageTask.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 375);
+            this.Controls.Add(this.button1);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Alerter.AlertControl alertControl1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+    }
+}
