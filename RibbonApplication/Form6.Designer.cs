d9f1058661646bd1b61025115bc5085c45b5f56b
Changweihua <Changweihua@outlook.com>
2012-11-28 11:00:02 +0800

    Usage of AlertControl

---
+++ RibbonApplication/Form6.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace RibbonApplication
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnOpen = new DevExpress.XtraBars.BarButtonItem();
+            this.btnClear = new DevExpress.XtraBars.BarButtonItem();
+            this.btnExit = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ApplicationButtonText = null;
+            // 
+            // 
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.ExpandCollapseItem.Name = "";
+            this.ribbonControl1.Images = this.imageList1;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.btnOpen,
+            this.btnClear,
+            this.btnExit});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 4;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
+            this.ribbonControl1.SelectedPage = this.ribbonPage1;
+            this.ribbonControl1.ShowExpandCollapseButton = DevExpress.Utils.DefaultBoolean.True;
+            this.ribbonControl1.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Show;
+            this.ribbonControl1.ShowToolbarCustomizeItem = false;
+            this.ribbonControl1.Size = new System.Drawing.Size(727, 147);
+            this.ribbonControl1.Toolbar.ShowCustomizeItem = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "FileOpen.png");
+            this.imageList1.Images.SetKeyName(1, "InkEraser.png");
+            this.imageList1.Images.SetKeyName(2, "GroupColorModeClose.png");
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Caption = "Open";
+            this.btnOpen.Id = 1;
+            this.btnOpen.ImageIndex = 0;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnOpen_ItemClick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Caption = "Clear";
+            this.btnClear.Id = 2;
+            this.btnClear.ImageIndex = 1;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnClear_ItemClick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Caption = "Exit";
+            this.btnExit.Id = 3;
+            this.btnExit.ImageIndex = 2;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnExit_ItemClick);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnOpen);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnClear);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnExit);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xml";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "XML|*.xml";
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 380);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "Form6";
+            this.Text = "Form6";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private System.Windows.Forms.ImageList imageList1;
+        private DevExpress.XtraBars.BarButtonItem btnOpen;
+        private DevExpress.XtraBars.BarButtonItem btnClear;
+        private DevExpress.XtraBars.BarButtonItem btnExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ RibbonApplication/Form6.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars.Ribbon;
+using System.Xml.Linq;
+using DevExpress.XtraBars;
+
+namespace RibbonApplication
+{
+    /*************************************************************************************
+     * CLR  版本:       4.0.30319.586
+     * 类 名 称:       Form6
+     * 机器名称:       HSERVER
+     * 命名空间:       RibbonApplication
+     * 文 件 名:       Form6
+     * 创建时间:       2012/11/28  8:47:10
+     * 作    者:       常伟华 Changweihua
+     * 签    名:       To be or not, it is not a problem !
+     * 网    站:       http://www.cmono.net
+     * 邮    箱:       changweihua@outlook.com
+     * 
+     * 修改时间:
+     * 修 改 人:
+     * 
+     ************************************************************************************/
+    /// <summary>
+    /// Create Ribbon Group dynamicly
+    /// </summary>
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void btnClear_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            while (ribbonPage1.Groups.Count > 1)
+            {
+                ribbonPage1.Groups.RemoveAt(1);
+            }
+
+            while (imageList1.Images.Count > 3)
+            {
+                imageList1.Images.RemoveAt(3);
+            }
+
+        }
+
+        private void btnOpen_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            btnClear_ItemClick(sender, e);
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                RibbonPageGroup dynamicGroup = new RibbonPageGroup("Dynamic");
+                ribbonPage1.Groups.Add(dynamicGroup);
+
+                XDocument xml = XDocument.Load(openFileDialog1.FileName);
+
+
+                var items = from item in xml.Elements("items").Elements("item")
+                            select item;
+
+                foreach (var item in items)
+                {
+                    string buttonName = item.Attribute("name").Value;
+                    string imageName = item.Attribute("image").Value;
+                    int imageIndex = imageList1.Images.Add(Image.FromFile(imageName), Color.Transparent);
+
+                    BarButtonItem button = new BarButtonItem(ribbonControl1.Manager, buttonName) { ImageIndex=imageIndex };
+
+                    dynamicGroup.ItemLinks.Add(button);
+
+                }
+
+            }
+
+        }
+    }
+}
